378137bb8ebd8de80b30dfa48ecf7a33026f524b
Arney <123242662+Arney1@users.noreply.github.com>
2025-02-18 14:49:26 +0700

    added hideDone, doneTodo, fixed errors

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import os
 import json
-from datetime import *
-from countdown import *
+import datetime
+# import countdown
 
 
 def clearTerminal():
@@ -18,13 +18,15 @@ def createTodo(todos, i):
     dateNow = datetime.datetime.now()
     timeleft = deadline - dateNow
 
-    todos.append({
-        "id": i,
-        "name": newTodo,
-        "deadline": deadline_str,
-        "timeLeft": str(timeleft),
-        "status": False
-    })
+    todos.append(
+        {
+            "id": i,
+            "name": newTodo,
+            "deadline": deadline_str,
+            "timeLeft": str(timeleft),
+            "status": False,
+        }
+    )
     # file = open("todo.json", "w")
     # json.dump(todos, file)
     # file.close()
@@ -33,17 +35,49 @@ def createTodo(todos, i):
     return todos, i
 
 
+def updateTodo(todos):
+    file = open("todo.json", "w")
+    json.dump(todos, file, indent=2)
+    file.close()
+
+
 # def deleteTodo(todos, todo):
 #     if todo in todos:
 #         file = open("todo.txt", "w")
 #
 def actionTodo(id, todos, idCount):
-    clearTerminal()
-    print("e untuk edit nama, d untuk delete, x untuk toggle done")
-    printTodo()
-    menuselect = input()
-    # if menuselect == 'e':
+    isRunning = True
+    while isRunning:
+        clearTerminal()
+        thisTodo = next((todo for todo in todos if todo["id"] == id), {})
+        printTodo(thisTodo)
+        print(
+            "e untuk edit nama, d untuk delete, x untuk toggle done, q untuk kembali ke halaman utama"
+        )
+        menuselect = input()
+        if menuselect == "e":
+            editTodo(todos, id)
+        # elif menuselect == "d":
+        #     deleteTodo(todos, id):
+        elif menuselect == "x":
+            doneTodo(todos, id)
+        elif menuselect == "q":
+            isRunning = False
+
+    updateTodo(todos)
     return todos, idCount
+
+
+def editTodo(todos, id):
+    for todo in todos:
+        if todo["id"] == id:
+            todo["name"] = input("Ganti nama TODO: ")
+
+
+def doneTodo(todos, id):
+    for todo in todos:
+        if todo["id"] == id:
+            todo["status"] = not todo["status"]
 
 
 #
@@ -57,8 +91,6 @@ def getTodos(todos, idCount):
     if len(todos) != 0:
         idCount = todos[len(todos) - 1]["id"]
 
-
-
     return todos, idCount
 
 
@@ -67,68 +99,85 @@ def printHeaderTodo():
         "No."
         + " | "
         + "Nama"
-        + "    | "
+        + " | "
         + "Status"
-        + "   | "
+        + " | "
         + "Deadline"
-        + "         | "
+        + " | "
         + "Time left"
     )
 
 
-def printTodo(todo, i):
-
+def printTodo(todo):
     print(
-
-        str(i)
-        + ". "
-        + " | "
+        "| "
         + todo["name"]
         + " | "
         + ("done" if todo["status"] else "not done")
         + " | "
         + (todo["timeLeft"] if todo["status"] else todo["deadline"])
         + " |"
-        + todo["timeLeft"] 
-
+        + todo["timeLeft"]
     )
-    i += 1
-        + ". "  # Period followed by a space for formatting
-        + " | "  # Separator
-        + todo["name"]  # Name of the todo item
-        + " | "  # Separator
-        + ("done" if todo["status"] else "not done")  # Status based on completion
-        + " | "  # Separator
-        + (todo["timeLeft"] if todo["status"] else todo["deadline"])  # Show time left if done, else show deadline
-        + " |"  # Separator
-        + todo["timeLeft"]  # Time left until deadline
-    return i
+    # i += 1
+    #     + ". "  # Period followed by a space for formatting
+    #     + " | "  # Separator
+    #     + todo["name"]  # Name of the todo item
+    #     + " | "  # Separator
+    #     + ("done" if todo["status"] else "not done")  # Status based on completion
+    #     + " | "  # Separator
+    #     + (todo["timeLeft"] if todo["status"] else todo["deadline"])  # Show time left if done, else show deadline
+    #     + " |"  # Separator
+    #     + todo["timeLeft"]  # Time left until deadline
 
 
 def printTodos(todos):
     print("TODO List:")
+    printHeaderTodo()
     i = 1
     for todo in todos:
-        i = printTodo(todo, i)
+        print(str(i) + ". ", end="")
+        printTodo(todo)
+        i += 1
+
+
+def filterTodo(todos, hide):
+    if hide:
+        todos = [todo for todo in todos if not todo["status"]]
+    return todos
 
 
 idCount = 0
 todos = []
 isRunning = True
-    
-    # Increment the serial number for the next todo item
+hideDone = False
+
+# Increment the serial number for the next todo item
 while isRunning:
-    return i  # Return the updated serial number
+    # Return the updated serial number
     todos, idCount = getTodos(todos, idCount)
+    todos = filterTodo(todos, hideDone)
     clearTerminal()
 
-    printHeaderTodo()
     printTodos(todos)
-    print("Menu: q untuk quit, pilih nomer untuk edit atau delete, c untuk create")
+    print(
+        "Menu:\n- q untuk quit\n- pilih nomer untuk edit, done, atau delete\n- c untuk create\n- "
+        + ("h untuk show done" if hideDone else "h untuk hide done")
+    )
     menuselect = input()
     if menuselect == "q":
         isRunning = False
     elif menuselect == "c":
         todos, idCount = createTodo(todos, idCount)
-    elif menuselect is int:
-        todos, idCount = actionTodo(menuselect, todos, idCount)
+    elif menuselect == "h":
+        hideDone = not hideDone
+    else:
+        err = False
+        try:
+            int(menuselect)
+            if (int(menuselect) > 0) and (int(menuselect) <= len(todos)):
+                id = todos[int(menuselect) - 1]["id"]
+                todos, idCount = actionTodo(id, todos, idCount)
+        except ValueError:
+            err = True
+            print("bruh")
